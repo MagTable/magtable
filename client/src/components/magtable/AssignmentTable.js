f9b603c5af2851d17a76f94148fb223f03936586
Arran Woodruff <arranwoodruff@gmail.com>
2020-02-04 12:11:01 -0700

    Added react-router-dom

---
+++ client/src/components/magtable/AssignmentTable.js
@@ -0,0 +1,28 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import {useLocation} from "react-router-dom";
+// import { connect } from 'react-redux'
+
+const AddUser = props => {
+    const location = useLocation();
+    return (
+        <div>
+            <h1>url: {location.pathname}</h1>
+        </div>
+    )
+}
+
+AddUser.propTypes = {
+
+}
+
+const mapStateToProps = (state) => ({
+
+})
+
+const mapDispatchToProps = {
+
+}
+
+// export default connect()(AddUser);
+export default AddUser;
