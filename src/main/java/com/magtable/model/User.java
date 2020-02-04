8d2673e365d0ada68ddb147b93f4284d0756e762
DavidWard98 <daeward98@gmail.com>
2020-02-04 11:32:26 -0700

    Created a SafeUser model -> Just user with passwords removed Updated UserController to use SafeUsers added application.properties to the .gitignore

--- src/main/java/com/magtable/model/User.java
+++ src/main/java/com/magtable/model/User.java
@@ -3,11 +3,18 @@ package com.magtable.model;
 import javax.persistence.*;
 import java.io.Serializable;
 
+
+/**
+ * Model class for user
+ * @author David
+ *
+ */
+
+//@TODO Class may change when we implement mustafas database.
+
 @Entity
 //@Table annotation is used to provide the details of the table that this entity will be mapped to
 @Table(name = "users")
-//Entity Listener is for if we want to auto populate some fields with jpa -> for example a date_created field
-//@EntityListeners(AuditingEntityListener.class)
 // ALL JAVA BEANS ARE SERIALIZABLE!
 public class User implements Serializable {
 
@@ -21,33 +28,15 @@ public class User implements Serializable {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    //@NotBlank annotation is used to validate that the annotated field is not null or empty.
-    // @TODO COULDN't GET NOTBLANK TO WORK FIX LATER
-    // @NotBlank
     private int userLevelID;
 
-    // @NotBlank
     private String username;
 
-    // @NotBlank
     private String password;
 
     //Emtpy Constructor that JPA uses
     protected User() {
     }
-
-    /**
-     * HELPER METHODS
-     **/
-
-    // @TODO I have no idea what this method is supposed to do
-    /*
-     * @ Above - Userlevel wouldn't necessarily be an attribute of User based on the ERD
-     * JPA would probably resolve this method on it's own so this method may not need to be implemented
-     */
-//    public String getUserLevel() {
-//        return "not implemented";
-//    }
 
     /**
      * GETTERS AND SETTERS
@@ -84,16 +73,5 @@ public class User implements Serializable {
     public void setPassword(String password) {
         this.password = password;
     }
-
-    /*
-    //@Column annotation is used to define the properties of the column that will be mapped to
-    // the annotated field. You can define several properties like name, length, nullable, updateable etc.
-    @Column(nullable = false, updatable = false)
-    //@Temporal annotation is used with java.util.Date and java.util.Calendar classes. It converts the date and time values from Java Object to compatible database type and vice versa.
-    @Temporal(TemporalType.TIMESTAMP)
-    @CreatedDate
-    private Date createdOn;
-*/
-
 
 }
