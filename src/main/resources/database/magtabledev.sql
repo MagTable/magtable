6f9a53b07d1cf4ac8e02ef321cd0bab9b2392754
mustafa <mustafa.alkhaldi@edu.sait.ca>
2020-02-04 20:30:52 -0700

    Database files for Use Case 1

---
+++ src/main/resources/database/magtabledev.sql
@@ -0,0 +1,20 @@
+DROP DATABASE IF EXISTS magtabledev;
+CREATE DATABASE magtabledev;
+
+USE magtabledev;
+
+CREATE TABLE `User` (
+    userID VARCHAR(32) NOT NULL,
+    levelID TINYINT(1) NOT NULL,
+    username VARCHAR(32) UNIQUE,
+    password VARCHAR(60) NULL NULL,
+    PRIMARY KEY (userID),
+    KEY `FK_levelID` (levelID),
+    CONSTRAINT `FK_levelID` FOREGIN KEY (levelID) REFERENCES `UserLevel` (levelID) ON DELETE RESTRICT ON UPDATE RESTRICT
+);
+
+CREATE TABLE `UserLevel` (
+    levelID TINYINT(1) NOT NULL,
+    description VARCHAR2(20),
+    PRIMARY KEY (levelID)
+);
