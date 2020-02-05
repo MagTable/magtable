ccfe5afc4eab8b178b4a258ea87b6bc581a0a17f
Arran Woodruff <arranwoodruff@gmail.com>
2020-02-04 21:21:01 -0700

    Redux Added Styled Component with props

---
+++ client/src/components/layout/Alert.js
@@ -0,0 +1,30 @@
+import React from "react";
+import PropTypes from "prop-types";
+import { connect } from "react-redux";
+import { AlertDiv as StyledAlertDiv, Alert as StyledAlert } from "../../styled/Alert";
+
+const Alert = ({ alerts }) => {
+  if (alerts !== null && alerts.length > 0) {
+    return (
+      <StyledAlertDiv>
+        {alerts.map(alert => (
+          <StyledAlert key={alert.id} alerttype={alert.alertType}>
+            {alert.msg}
+          </StyledAlert>
+        ))}
+      </StyledAlertDiv>
+    );
+  } else {
+    return null;
+  }
+};
+
+Alert.propTypes = {
+  alerts: PropTypes.array.isRequired
+};
+
+const mapStateToProps = state => ({
+  alerts: state.alert
+});
+
+export default connect(mapStateToProps)(Alert);
