58e4e9ed1ae2b4a2fcffaf5f31f6c7e3c0bc253f
mustafa <mustafa.alkhaldi@edu.sait.ca>
2020-02-04 20:30:52 -0700

    Database files for Use Case 1

---
+++ src/main/resources/database/magtableinsert.sql
@@ -0,0 +1,7 @@
+INSERT INTO `UserLevel` VALUES (1, 'System Manager');
+INSERT INTO `UserLevel` VALUES (2, 'Personnel Manager');
+INSERT INTO `UserLevel` VALUES (3, 'Mechanic');
+
+INSERT INTO `User` VALUES(1, 1, 'mustafa', 'password');
+INSERT INTO `User` VALUES(2, 2, 'david', 'password');
+INSERT INTO `User` VALUES(3, 3, 'steven', 'password');
