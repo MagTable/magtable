230fb89cd984e45f7ff71c9a1f131edde30462cf
MJ Kochuk <mjkochuk@gmail.com>
2020-02-05 00:19:04 -0700

    Added code templates, documentation and documentation script now available.

---
+++ client/src/styled/HelloComponent.js
@@ -0,0 +1,16 @@
+import styled from "styled-components";
+
+/*
+    Date: 2020-02-04
+    Author: mjjmk 
+*/
+
+/**
+ * HelloComponent - Test to see if the code template works!
+ * @module Styled
+ **/
+const HelloComponent = styled.h1`
+    width: 100px
+    `;
+
+export default HelloComponent;
