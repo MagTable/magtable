e1a18d7bc2e11d6f74a221841880816c56c929f7
Steven Wong <stevenvono@gmail.com>
2020-02-09 02:44:08 -0700

    Added in Tom's work, will test in the morning.

--- client/src/components/user/AddUser.js
+++ client/src/components/user/AddUser.js
@@ -1,21 +1,44 @@
-import React from 'react';
-import { useLocation } from 'react-router-dom';
-// import { connect } from 'react-redux'
+import React, {useState} from 'react';
+import { connect } from 'react-redux'
+import {addUser} from '../../actions/user'
+import {useDispatch} from "react-redux";
 
-const AddUser = props => {
-	const location = useLocation();
+const AddUser = ({addUser}) => {
+	const [role, setRole] = useState("2");
+	const [username, setUsername] = useState("");
+	const [tempPassword, setTempPassword] = useState("");
+	const dispatch = useDispatch();
+
+	function handleSubmit(e) {
+		e.preventDefault();
+		const user = {
+			roleID: role,
+			username: username,
+			password: tempPassword
+		}
+		addUser(user);
+		setRole("2");
+		setUsername("");
+		setTempPassword("");
+	}
+
 	return (
-		<div>
-			<h1>url: {location.pathname}</h1>
-		</div>
+		<form onSubmit={ (e) => handleSubmit(e) }>
+			<select name="role" value={role}
+					onChange={(e) => setRole(e.target.value)}>
+				<option value="1">System Manager</option>
+				<option value="2" selected>Personnel Manager</option>
+				<option value="3">Mechanic</option>
+			</select>
+			<input type="text" name="username" placeholder="Username" value={username} required
+				   onChange={(e) => setUsername(e.target.value)}/>
+			<input type="text" name="tempPassword" placeholder="Temp password" value={tempPassword} required
+				   onChange={(e) => setTempPassword(e.target.value)}/>
+			<input type="submit"/>
+		</form>
 	);
 };
 
 AddUser.propTypes = {};
 
-const mapStateToProps = state => ({});
-
-const mapDispatchToProps = {};
-
-// export default connect()(AddUser);
-export default AddUser;
+export default connect(null, {addUser})(AddUser);
