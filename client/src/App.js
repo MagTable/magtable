2c1da7a08259d2ae89eb3aab4fa427315b15b764
MJ Kochuk <mjkochuk@gmail.com>
2020-02-12 19:40:53 -0700

    Removed extraneous components.

--- client/src/App.js
+++ client/src/App.js
@@ -6,24 +6,16 @@ import store from "./store";
 import { loadUser } from "./actions/auth";
 
 import PrivateRoute from "./components/auth/PrivateRoute";
-import Alert from "./components/common/Alert";
-import NavBar from "./components/_dumbcomponents/NavBar";
 import Login from "./components/auth/Login";
-import UserList from "./components/user/UserList";
 import Logout from "./components/auth/Logout";
-import AssignmentTable from "./components/magtable/AssignmentTable";
 import PasswordReset from "./components/auth/PasswordReset";
 import { setAlert } from "./actions/alert";
-import React, {useState} from 'react';
-import './App.css';
-import { BrowserRouter as Router, Link, Route, Switch } from 'react-router-dom';
-import UserList from './components/user/UserList';
-import AddUser from './components/user/AddUser';
-import AssignmentTable from './components/magtable/AssignmentTable';
-import { Provider } from 'react-redux';
-import store from './store';
-import Alert from './components/common/Alert';
-import NavBar from './components/_dumbcomponents/NavBar';
+import "./App.css";
+import UserList from "./components/user/UserList";
+import AddUser from "./components/user/AddUser";
+import AssignmentTable from "./components/magtable/AssignmentTable";
+import Alert from "./components/common/Alert";
+import NavBar from "./components/_dumbcomponents/NavBar";
 
 function App() {
 	useEffect(() => {
@@ -40,30 +32,7 @@ function App() {
 	return (
 		<Provider store={store}>
 			<Router>
-				<NavBar/>
-				<Alert/>
-				{/*<ul>*/}
-				{/*    <li>*/}
-				{/*        <Link to="/">/</Link>*/}
-				{/*    </li>*/}
-				{/*    <li>*/}
-				{/*        <Link to="/user/all">/user/all</Link>*/}
-				{/*    </li>*/}
-				{/*    <li>*/}
-				{/*        <Link to="/user/insert">/user/insert</Link>*/}
-				{/*    </li>*/}
-				{/*</ul>*/}
-				{/*<Switch>*/}
-				{/*    <Route exact path="/">*/}
-				{/*        <AssignmentTable/>*/}
-				{/*    </Route>*/}
-				{/*    <Route exact path="/user/all">*/}
-				<UserList />
-				{/*    </Route>*/}
-				{/*    <Route exact path="/user/insert">*/}
-				{/*        <AddUser/>*/}
-				{/*    </Route>*/}
-				{/*</Switch>*/}
+				<Alert />
 				<NavBar />
 				<Alert />
 				<Switch>
@@ -81,6 +50,7 @@ function App() {
 						component={UserList}
 						adminRoute
 					/>
+					{/*<PrivateRoute exact path="/users" component={UserList}/>*/}
 					<PrivateRoute exact path="/logout" component={Logout} />
 				</Switch>
 			</Router>
