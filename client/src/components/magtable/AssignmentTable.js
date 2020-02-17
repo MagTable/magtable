79d6290c0ef94888a4c21d98535e52f9b10b7e6d
MJ Kochuk <mjkochuk@gmail.com>
2020-02-17 15:20:08 -0700

    Added a title component for the different sections of the mag table. Not completed since flex is treating the parking locations title and tower title as equal elements to the other children.

--- client/src/components/magtable/AssignmentTable.js
+++ client/src/components/magtable/AssignmentTable.js
@@ -3,13 +3,13 @@ import {EmployeeList, EmployeeListItem} from "../../styled/magtable/Employee";
 import {TruckList} from "../../styled/magtable/Truck";
 import {
 	AssignmentContainer,
-	MapsContainer,
 	MapsDiv,
 	TowerMap,
 	TowerPosition,
 	TruckMap
 } from "../../styled/magtable/Maps";
 import TruckListItem from "./TruckListItem";
+import {ListTitle, ListTitleText} from "../../styled/magtable/Titling";
 
 /**
  * Placeholder component to assist in displaying routing
@@ -19,17 +19,39 @@ const AssignmentTable = () => {
 	return (
 		<AssignmentContainer>
 				<EmployeeList>
+					<ListTitle>
+						<ListTitleText>
+							Employees
+						</ListTitleText>
+					</ListTitle>
+
 					<EmployeeListItem/>
 					<EmployeeListItem/>
 				</EmployeeList>
 				<TruckList>
+					<ListTitle>
+						<ListTitleText>
+							Trucks
+						</ListTitleText>
+					</ListTitle>
 					<TruckListItem/>
 					<TruckListItem/>
 				</TruckList>
 
 				<MapsDiv>
-					<TruckMap/>
+					<TruckMap>
+						<ListTitle>
+							<ListTitleText>
+								Parking Locations
+							</ListTitleText>
+						</ListTitle>
+					</TruckMap>
 					<TowerMap>
+						<ListTitle>
+							<ListTitleText>
+								Tower
+							</ListTitleText>
+						</ListTitle>
 						<TowerPosition/>
 						<TowerPosition/>
 						<TowerPosition/>
---
+++ client/src/styled/magtable/Titling.js
@@ -0,0 +1,24 @@
+import styled from "styled-components";
+
+/**
+ * @date 2020-02-17
+ * @author MJ Kochuk
+ * @module Styled
+ */
+
+/**
+ *
+ **/
+export const ListTitle = styled.div`
+    background-color: grey;
+    // width: 100%
+   
+`;
+
+export const ListTitleText = styled.p`
+    // padding: 2px;
+    // margin: 0px;
+    padding-left: 30px;
+    font-size: 20px;
+    font-weight: bold;
+`;
