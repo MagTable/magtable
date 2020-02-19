4f126170afba9a8d23e7ec7e07df78ac1d55f1d7
Arran Woodruff <arranwoodruff@gmail.com>
2020-02-19 09:12:32 -0700

    Actions first draft + Test data added to reducer

--- client/src/reducers/magtable.js
+++ client/src/reducers/magtable.js
@@ -2,7 +2,6 @@ import {
 	ADD_BRIX_RECORD,
 	ADD_DAILY_MESSAGE,
 	ADD_EMPLOYEE_SHIFT,
-	ADD_EQUIPMENT_EMPLOYEE,
 	EAST_APRON,
 	GET_ASSIGNMENT_DATA,
 	PUBLISH_TABLE,
@@ -22,6 +21,7 @@ import {
  * {
  * 	equipment: {
  * 		id: String,
+ * 		label: String,
  * 		notice: String,
  * 		status: String,
  * 	}
@@ -47,15 +47,225 @@ import {
  * }
  */
 
+const initialAssignments = [
+	{
+		equipment: {
+			id: 1,
+			label: 24,
+			notice: "Equipment Conditional Status Message",
+			status: "CON"
+		},
+		employeeShifts: [],
+		parkingLocation: null,
+		brixRecords: []
+	},
+	{
+		equipment: {
+			id: 2,
+			label: 26,
+			notice: "",
+			status: "GO"
+		},
+		employeeShifts: [],
+		parkingLocation: null,
+		brixRecords: [
+			{
+				nozzle: 24.5,
+				t1Tank: 52.3,
+				t4Tank: 32.4,
+				litersPurged: 80,
+				timeMeasured: new Date()
+			}
+		]
+	},
+	{
+		equipment: {
+			id: 3,
+			label: 32,
+			notice: "",
+			status: "INOP"
+		},
+		employeeShifts: [],
+		parkingLocation: null,
+		brixRecords: []
+	},
+	{
+		equipment: {
+			id: 3,
+			label: 36,
+			notice: "",
+			status: "OOS"
+		},
+		employeeShifts: [],
+		parkingLocation: null,
+		brixRecords: []
+	}
+];
+
+const initialEmployeeShifts = [
+	{
+		id: 1,
+		name: "MJ Kochuk",
+		startTime: 400,
+		endTime: 1600,
+		labels: ["gp", "ts"]
+	},
+	{
+		id: 2,
+		name: "Arran Woodruff",
+		startTime: 400,
+		endTime: 1600,
+		labels: ["ojt"]
+	},
+	{
+		id: 3,
+		name: "Mustafa Al Khaldi",
+		startTime: 400,
+		endTime: 1600,
+		labels: ["ts"]
+	},
+	{
+		id: 4,
+		name: "Steven Wong",
+		startTime: 400,
+		endTime: 1600,
+		labels: ["bl"]
+	},
+	{
+		id: 5,
+		name: "Tom Allcock",
+		startTime: 600,
+		endTime: 1200,
+		labels: ["bl"]
+	},
+	{
+		id: 6,
+		name: "David Ward",
+		startTime: 600,
+		endTime: 1200,
+		labels: ["ojt", "gp"]
+	},
+	{
+		id: 7,
+		name: "MJ Kochuk",
+		startTime: 600,
+		endTime: 1200,
+		labels: ["gp", "ts"]
+	},
+	{
+		id: 8,
+		name: "Arran Woodruff",
+		startTime: 600,
+		endTime: 1200,
+		labels: ["ojt"]
+	},
+	{
+		id: 9,
+		name: "Mustafa Al Khaldi",
+		startTime: 1430,
+		endTime: 230,
+		labels: ["ts"]
+	},
+	{
+		id: 10,
+		name: "Steven Wong",
+		startTime: 1430,
+		endTime: 230,
+		labels: ["bl"]
+	},
+	{
+		id: 11,
+		name: "Tom Allcock",
+		startTime: 1430,
+		endTime: 230,
+		labels: ["bl"]
+	},
+	{
+		id: 12,
+		name: "David Ward",
+		startTime: 1430,
+		endTime: 230,
+		labels: ["ojt", "gp"]
+	}
+];
+
+const initialDailyMessages = [
+	"Make sure to ______",
+	"Dont forget your _____",
+	"Slippery ice at _____"
+];
+
+const initialDailyMix = 45;
+
+const initialParkingLocations = [
+	{ id: 1, apron: "WDA", code: "AW1" },
+	{ id: 2, apron: "WDA", code: "AE1" },
+	{ id: 3, apron: "WDA", code: "BW1" },
+	{ id: 4, apron: "WDA", code: "BE1" },
+	{ id: 5, apron: "WDA", code: "BW2" },
+	{ id: 6, apron: "WDA", code: "BE2" },
+	{ id: 7, apron: "WDA", code: "CE2" },
+	{ id: 8, apron: "WDA", code: "CW2" },
+	{ id: 9, apron: "WDA", code: "CE3" },
+	{ id: 10, apron: "WDA", code: "CW3" },
+	{ id: 11, apron: "WDA", code: "D4" },
+	{ id: 12, apron: "EDA", code: "AE1" },
+	{ id: 13, apron: "EDA", code: "AC1" },
+	{ id: 14, apron: "EDA", code: "AW1" },
+	{ id: 15, apron: "EDA", code: "BE1" },
+	{ id: 16, apron: "EDA", code: "BC1" },
+	{ id: 17, apron: "EDA", code: "BW1" },
+	{ id: 18, apron: "EDA", code: "BE3" },
+	{ id: 19, apron: "EDA", code: "BC3" },
+	{ id: 20, apron: "EDA", code: "BW3" },
+	{ id: 21, apron: "EDA", code: "CE3" },
+	{ id: 22, apron: "EDA", code: "CC3" },
+	{ id: 23, apron: "EDA", code: "CW3" },
+	{ id: 24, apron: "EDA", code: "CE4" },
+	{ id: 25, apron: "EDA", code: "CC4" },
+	{ id: 26, apron: "EDA", code: "CW4" },
+	{ id: 27, apron: "EDA", code: "DE4" },
+	{ id: 28, apron: "EDA", code: "DC4" },
+	{ id: 29, apron: "EDA", code: "DW4" },
+	{ id: 30, apron: "EDA", code: "DE6" },
+	{ id: 31, apron: "EDA", code: "DC6" },
+	{ id: 32, apron: "EDA", code: "DW6" },
+	{ id: 33, apron: "EDA", code: "EE6" },
+	{ id: 34, apron: "EDA", code: "EC6" },
+	{ id: 35, apron: "EDA", code: "EW6" },
+	{ id: 36, apron: "EDA", code: "EE7" },
+	{ id: 37, apron: "EDA", code: "EC7" },
+	{ id: 38, apron: "EDA", code: "EW7" },
+	{ id: 39, apron: "EDA", code: "FE7" },
+	{ id: 40, apron: "EDA", code: "FC7" },
+	{ id: 41, apron: "EDA", code: "FW7" },
+	{ id: 42, apron: "EDA", code: "FE9" },
+	{ id: 43, apron: "EDA", code: "FC9" },
+	{ id: 44, apron: "EDA", code: "FW9" },
+	{ id: 45, apron: "EDA", code: "GE9" },
+	{ id: 46, apron: "EDA", code: "GC9" },
+	{ id: 47, apron: "EDA", code: "GW9" }
+];
+
 const initialState = {
-	assignments: [],
-	employeeShifts: [],
-	dailyMessages: [],
-	parkingLocations: [],
-	dailyMix: null,
+	assignments: initialAssignments,
+	employeeShifts: initialEmployeeShifts,
+	dailyMessages: initialDailyMessages,
+	parkingLocations: initialParkingLocations,
+	dailyMix: initialDailyMix,
 	selectedApron: EAST_APRON,
-	loading: true
+	loading: false
 };
+
+// const initialState = {
+// 	assignments: [],
+// 	employeeShifts: [],
+// 	dailyMessages: [],
+// 	parkingLocations: [],
+// 	dailyMix: null,
+// 	selectedApron: EAST_APRON,
+// 	loading: true
+// };
 
 export default function(state = initialState, action) {
 	const { type, payload } = action;
@@ -65,7 +275,7 @@ export default function(state = initialState, action) {
 			return {
 				...state,
 				assignments: state.assignments.map(assignment =>
-					assignment.equipment.id === payload.equipmentID
+					assignment.equipment.id === payload // equipmentID
 						? {
 								...assignment,
 								parkingLocation: null
@@ -99,30 +309,33 @@ export default function(state = initialState, action) {
 						: assignment
 				)
 			};
-		case ADD_EQUIPMENT_EMPLOYEE: // if assignment exists already
+		// case ADD_EQUIPMENT_EMPLOYEE: // if assignment exists already
+		// 	return {
+		// 		...state,
+		// 		assignments: state.assignments.map(assignment =>
+		// 			assignment.equipment.id === payload.equipmentID
+		// 				? {
+		// 						...assignment,
+		// 						employeeShifts: [...assignment.employeeShifts, payload.shift]
+		// 				  }
+		// 				: assignment
+		// 		)
+		// 	};
+		case SET_EQUIPMENT_EMPLOYEE: // if assignment needs to be created
+			const modifiedAssignment = state.assignments.find(
+				assignment => assignment.equipment.id === payload.equipmentID
+			);
+
+			modifiedAssignment.splice(payload.equipmentSlotID, 0, payload.shift);
+			// assignment.employeeShifts[payload.equipmentSlotID] = payload.shift;
+
 			return {
 				...state,
 				assignments: state.assignments.map(assignment =>
 					assignment.equipment.id === payload.equipmentID
-						? {
-								...assignment,
-								employeeShifts: [...assignment.employeeShifts, payload.shift]
-						  }
+						? modifiedAssignment
 						: assignment
 				)
-			};
-		case SET_EQUIPMENT_EMPLOYEE: // if assignment needs to be created
-			return {
-				...state,
-				assignments: [
-					...state.assignments,
-					{
-						equipment: payload.equipment,
-						employeeShifts: [payload.shift],
-						parkingLocation: null,
-						brixRecords: []
-					}
-				]
 			};
 		case PUBLISH_TABLE:
 			return {
@@ -133,10 +346,10 @@ export default function(state = initialState, action) {
 			return {
 				...state,
 				assignments: state.assignments.map(assignment =>
-					assignment.equipment.id === payload.truckID
+					assignment.equipment.id === payload.equipmentID
 						? {
 								...assignment,
-								brixRecords: [...assignment.brixRecords, payload.brixRecord]
+								brixRecords: payload // update list of brixRecords from API
 						  }
 						: assignment
 				)
@@ -174,7 +387,7 @@ export default function(state = initialState, action) {
 		case ADD_EMPLOYEE_SHIFT:
 			return {
 				...state,
-				employeeShifts: [...state.employeeShifts, payload]
+				employeeShifts: payload
 			};
 		case TOGGLE_BAY_LEAD:
 			// toggles the isBayLead attribute of the specified truck according to payload (id)
