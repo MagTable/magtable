9c503d731dbbad59e862567de2ed2f114286615f
David Ward <david.a.ward@edu.sait.ca>
2020-02-20 23:15:37 -0700

    updated a route comment
    Signed-off-by: David Ward <david.a.ward@edu.sait.ca>

--- src/main/java/com/magtable/controller/ShiftController.java
+++ src/main/java/com/magtable/controller/ShiftController.java
@@ -36,17 +36,18 @@ public class ShiftController {
      * description     route to force a new fetch of W2W shift data
      * access          Personnel Managers, System Admins
      *
+     * @return an OK status if routes were updated
      */
     @PostMapping("/update")
-    public void updateShifts(){
+    public ResponseEntity<HttpStatus> updateShifts(){
         ShiftScheduler shiftScheduler = ShiftScheduler.getInstance();
         try {
             shiftScheduler.updateShifts();
         } catch (Exception e) {
             throw errorService.sessionExpired();
         }
+        return ResponseEntity.ok(HttpStatus.OK);
     }
-
 
     //TODO discuss with arran -> I don't know if this is what we need. I assume the SID will be gotten from the front end
     @PostMapping("/update/SID")
@@ -54,8 +55,8 @@ public class ShiftController {
         ShiftScheduler shiftScheduler = ShiftScheduler.getInstance();
         shiftScheduler.setSID(SID);
         return ResponseEntity.ok(HttpStatus.OK);
-
     }
+
 
 
 
