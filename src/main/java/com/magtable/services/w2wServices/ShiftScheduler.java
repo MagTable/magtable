f7eeb155752582e8a10b75c457b40d1317857ee0
David Ward <david.a.ward@edu.sait.ca>
2020-02-20 23:15:24 -0700

    changed cron to 2am
    Signed-off-by: David Ward <david.a.ward@edu.sait.ca>

--- src/main/java/com/magtable/services/w2wServices/ShiftScheduler.java
+++ src/main/java/com/magtable/services/w2wServices/ShiftScheduler.java
@@ -56,7 +56,7 @@ public class ShiftScheduler {
     //every 5 seconds
    // @Scheduled(cron = "*/5 * * * * *")
     //everyday at 2Am
-    @Scheduled(cron = "0 0 2,22 * * *")
+    @Scheduled(cron = "0 0 2 * * *")
     public void updateShifts() throws Exception {
        ShiftScheduler shiftScheduler = getInstance();
        shiftScheduler.setShiftList(new ArrayList());
