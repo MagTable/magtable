652d94f9955fe3d07421db70755f35c3fe2b2b82
DavidWard98 <45475170+DavidWard98@users.noreply.github.com>
2020-02-25 09:19:37 -0700

    Deleted database models and repos

--- src/main/java/com/magtable/repository/EquipmentRepository.java
+++
@@ -1,9 +0,0 @@
-package com.magtable.repository;
-
-import com.magtable.model.Equipment;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
-
-public interface EquipmentRepository extends JpaRepository<Equipment, Integer>, JpaSpecificationExecutor<Equipment> {
-
-}
