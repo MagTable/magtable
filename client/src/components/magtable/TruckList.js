df18efc5346c04232cff2526a8a7bb49e89fe577
Arran Woodruff <arranwoodruff@gmail.com>
2020-02-27 19:23:51 -0700

    Renamed TruckMap -> LocationMap

--- client/src/components/magtable/TruckList.js
+++ client/src/components/magtable/TruckList.js
@@ -2,7 +2,6 @@ import React, { useState } from "react";
 import { ListTitle, ListTitleText } from "../../styled/magtable/Titling";
 import TruckListItem from "../magtable/TruckListItem";
 import {
-	TruckListButton,
 	TruckListDiv,
 	TruckListDivWrapper,
 	TruckListManipDiv
@@ -26,7 +25,7 @@ import {
  * Rendered on the Truck Assignment page, displays all available trucks, the employees assigned to each (two AM and two
  * PM slots for employees) while color-coding each truck to represent their operational status. User can expand and
  * contract notices on all trucks and swap between displaying AM employees and PM employees. Trucks are draggable for
- * assigning them to locations in the TruckMap.
+ * assigning them to locations in the LocationMap.
  *
  * @constructor
  * @returns {*} The TruckList component
