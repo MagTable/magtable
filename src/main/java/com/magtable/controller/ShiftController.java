6cb4d9de755ec13d1f42bac53ed47215cdd0de60
David Ward <david.a.ward@edu.sait.ca>
2020-03-02 21:18:35 -0700

    Code cleanup
    Signed-off-by: David Ward <david.a.ward@edu.sait.ca>

--- src/main/java/com/magtable/controller/ShiftController.java
+++ src/main/java/com/magtable/controller/ShiftController.java
@@ -11,8 +11,10 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.io.*;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Calendar;
+import java.util.Date;
 
 @RestController
 @RequestMapping("/shift")
@@ -36,20 +38,12 @@ public class ShiftController {
         return shiftList;
     }
 
-    /**
-     * route           GET /shift/update
-     * description     route to force a new fetch of W2W shift data
-     * access          Personnel Managers, System Admins
-     *
-     * @return an OK status if routes were updated
-     */
+    //todo replace this in production
     @GetMapping("/update")
     public ShiftList updateShifts() {
-
         ShiftScheduler shiftScheduler = ShiftScheduler.getInstance();
         ShiftList shiftList = new ShiftList(shiftScheduler.getShiftList());
 
-        //todo remove later
         try {
             FileInputStream fileInputStream = new FileInputStream(new File("./src/main/java/com/magtable/controller/shiftlist.ser"));
             ObjectInputStream ois = new ObjectInputStream(fileInputStream);
@@ -57,39 +51,8 @@ public class ShiftController {
         } catch (Exception e) {
             e.printStackTrace();
         }
-        //todo remove comments
-      /*  boolean update = true;
 
-        String lastUpdated = shiftScheduler.getLastUpdated();
-        String currentTime = sdf.format(new Date());
-
-        if (lastUpdated != null) {
-            try {
-                // https://mkyong.com/java/how-to-calculate-date-time-difference-in-java/
-
-                Date d1 = sdf.parse(lastUpdated);
-                Date d2 = sdf.parse(currentTime);
-                long diff = d2.getTime() - d1.getTime();
-                long diffMinutes = diff / (60 * 1000) % 60;
-                if (diffMinutes < 10)
-                    update = false;
-
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-        }
-        if (update) {
-            try {
-                shiftScheduler.updateShifts();
-                shiftList.setLastUpdated(shiftScheduler.getLastUpdated());
-            } catch (Exception e) {
-                throw errorService.sessionExpired();
-            }
-        }
-        ///update for UI
- **/
         Calendar cal = Calendar.getInstance();
-        shiftScheduler.setLastUpdatedUI(String.format("%d:%02d", cal.get(Calendar.HOUR_OF_DAY), cal.get(Calendar.MINUTE)));
         shiftList.setLastUpdated(String.format("%d:%02d", cal.get(Calendar.HOUR_OF_DAY), cal.get(Calendar.MINUTE)));
         shiftScheduler.setShiftList(shiftList.getShifts());
         return shiftList;
@@ -130,4 +93,57 @@ public class ShiftController {
         return shiftList;
     }
 
+      //TODO PUT THIS IN IN PRODUCTION
+
+    /**
+     * * route           GET /shift/update
+     * * description     route to force a new fetch of W2W shift data
+     * * access          Personnel Managers, System Admins
+     * *
+     * * @return an OK status if routes were updated
+     *
+    @GetMapping("/update")
+    public ShiftList updateShifts() {
+        boolean update = true;
+
+        ShiftScheduler shiftScheduler = ShiftScheduler.getInstance();
+        ShiftList shiftList = new ShiftList(shiftScheduler.getShiftList());
+
+        SimpleDateFormat sdf = new SimpleDateFormat("HH:mm");
+        String lastUpdated = shiftScheduler.getLastUpdated();
+        String currentTime = sdf.format(new Date());
+
+        if (lastUpdated != null) {
+            try {
+                // https://mkyong.com/java/how-to-calculate-date-time-difference-in-java/
+
+                Date d1 = sdf.parse(lastUpdated);
+                Date d2 = sdf.parse(currentTime);
+                long diff = d2.getTime() - d1.getTime();
+                long diffMinutes = diff / (60 * 1000) % 60;
+                if (diffMinutes < 10)
+                    update = false;
+
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+        if (update) {
+            try {
+                shiftScheduler.updateShifts();
+                shiftList.setLastUpdated(shiftScheduler.getLastUpdated());
+            } catch (Exception e) {
+                throw errorService.sessionExpired();
+            }
+        }
+        ///update for UI
+        Calendar cal = Calendar.getInstance();
+        shiftScheduler.setLastUpdatedUI(String.format("%d:%02d", cal.get(Calendar.HOUR_OF_DAY), cal.get(Calendar.MINUTE)));
+
+        shiftList.setShifts(shiftScheduler.getShiftList());
+        shiftList.setLastUpdated(shiftScheduler.getLastUpdatedUI());
+        return shiftList;
+
+    }
+    **/
 }
