986ff4e7daaf5db879ba0e3e6cd10cac186721f1
David Ward <david.a.ward@edu.sait.ca>
2020-03-13 22:09:59 -0600

    Added Constructor that converts W2WShifts to ShiftResponses for the List
    Signed-off-by: David Ward <david.a.ward@edu.sait.ca>

--- src/main/java/com/magtable/model/ShiftList.java
+++ src/main/java/com/magtable/model/ShiftList.java
@@ -3,29 +3,54 @@ package com.magtable.model;
 import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.Calendar;
+import java.util.List;
 
 public class ShiftList implements Serializable {
 
     private String scheduleDate;
-    private String lastUpdated;
-    private ArrayList<ShiftResponse> shifts;
+    private static String lastUpdated;
+    private List<ShiftResponse> shifts;
 
+    public ShiftList(){}
 
-    public ShiftList(ArrayList<ShiftResponse> list) {
+    public ShiftList(ArrayList<W2WShift> list) {
         Calendar cal = Calendar.getInstance();
         this.scheduleDate = String.format("%d/%d/%d", cal.get(Calendar.YEAR), cal.get(Calendar.MONTH)+1, cal.get(Calendar.DATE));
-        this.shifts = list;
+
+        ArrayList<ShiftResponse> shiftResponses = new ArrayList<>();
+
+        //ShiftTimes need to be sent as a 0400 or 1600 to the front end (Client uses 24hr times)
+        for(W2WShift shift : list){
+            ShiftResponse shiftResponse = new ShiftResponse(shift);
+
+            // 2020-03-13 04:00:00.0
+            String startTimeStampString = shift.getStartTime().toString();
+            String[] splittedTime = startTimeStampString.split(" ");
+            String startTime = splittedTime[1].replace(":", "").substring(0,4);
+
+            String endTimeStampString = shift.getEndTime().toString();
+            splittedTime = endTimeStampString.split(" ");
+            String endTime = splittedTime[1].replace(":", "").substring(0,4);
+
+            shiftResponse.setStartTime(startTime);
+            shiftResponse.setEndTime(endTime);
+
+            shiftResponses.add(shiftResponse);
+
+        }
+
+        this.shifts = shiftResponses;
     }
 
     public String getScheduleDate() {
         return scheduleDate;
     }
 
-    public ArrayList<ShiftResponse> getShifts() {
+    public List<ShiftResponse> getShifts() {
         return shifts;
     }
 
-    public void setShifts(ArrayList<ShiftResponse> shifts) {
+    public void setShifts(List<ShiftResponse> shifts) {
         this.shifts = shifts;
     }
 
