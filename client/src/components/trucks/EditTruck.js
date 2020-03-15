d2deaab8cc5398ad121c86cacf17ca290868491f
MJ Kochuk <mjkochuk@gmail.com>
2020-03-15 16:24:18 -0600

    All values are now displayed correctly in the edit truck modal in manage trucks.

--- client/src/components/trucks/EditTruck.js
+++ client/src/components/trucks/EditTruck.js
@@ -1,13 +1,14 @@
 import React from "react";
 import { useDispatch } from "react-redux";
-import { Form, Formik } from "formik";
+import { Field, Form, Formik } from "formik";
 import * as Yup from "yup";
-import { editTruck } from "../../actions/truck";
+import { addTruck, editTruck } from "../../actions/truck";
 import { TRUCK_STATUSES } from "../../actions/constants";
 import SelectBox from "../common/SelectBox";
 import { LoginBtn } from "../../styled/auth/Login";
 import { AddTruckWrap } from "../../styled/trucks/TruckManagement";
 import TextArea from "../common/TextArea";
+import TextInput from "../common/TextInput";
 
 /**
  * @date 3/08/2020
@@ -17,7 +18,6 @@ import TextArea from "../common/TextArea";
 
 /**
  * For editing a truck in the manage trucks page.
- *
  * @constructor
  * @returns {*} The AddTruck component.
  */
@@ -61,26 +61,24 @@ const EditTruck = ({ truck }) => {
 		<AddTruckWrap>
 			<Formik
 				initialValues={{
-					id: truck.id === null ? "" : truck.id,
-					status: truck.status === null ? "" : truck.status,
-					notice: truck.notice === null ? "" : truck.notice
+					status: truck.status,
+					notice: truck.notice.text
 				}}
-				onSubmit={() => console.log("hello")}
+				onSubmit={(values, { resetForm }) => {
+					console.log(values);
+					resetForm();
+				}}
+				// onSubmit={handleEdit}
 				validationSchema={Yup.object().shape({
-					id: Yup.string()
-						.matches(/\d/, "Invalid Number")
-						.required("Required Field"),
-					status: Yup.string()
-						.oneOf(truckStatuses)
-						.required(),
+					status: Yup.string().oneOf(truckStatuses),
+					// .required(),
 					notice: Yup.string().max(250, "Maximum Length is 250 Characters")
 				})}
 			>
 				<Form>
 					<h2>Edit Truck</h2>
 					<h3>Truck {truck.id}</h3>
-					<select label="Truck Status" name="status">
-						<option value="" />
+					<select label="Truck Status" name="status" value={truck.status}>
 						{truckStatuses.map(truckStatus => {
 							return truck.status === truckStatus ? (
 								<option key={truckStatus} value={truckStatus} selected={true}>
@@ -93,13 +91,20 @@ const EditTruck = ({ truck }) => {
 							);
 						})}
 					</select>
-					<TextArea
-						label="Notice"
-						name="notice"
-						rows="6"
-						placeholder="Any truck notices go here..."
-						value={truck.notice}
-					/>
+
+					<Field name="notice">
+						{({ field }) => (
+							<TextArea
+								{...field}
+								label="Notice"
+								name="notice"
+								rows="6"
+								placeholder="Any truck notices go here..."
+								value={truck.notice}
+							/>
+						)}
+					</Field>
+
 					<br />
 					<LoginBtn type="submit">Save</LoginBtn>
 				</Form>
