ce6a36adb19de5fbc58fb843fc29bf86eab4eb48
Steven Wong <stevenvono@gmail.com>
2020-03-23 23:55:13 -0600

    Changed logo to no-text-logo for now.

--- client/src/components/common/NavBar.js
+++ client/src/components/common/NavBar.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import logo from "../../res/Images/WhiteTextLogo.svg";
+import logo from "../../res/Images/NoTextLogo.svg";
 import {
 	AeroLogo,
 	NavBar as NavBarDiv,
