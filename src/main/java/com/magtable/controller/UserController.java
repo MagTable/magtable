05f817d365f4f1c7032d9b9a3724fd6b428044af
David Ward <david.a.ward@edu.sait.ca>
2020-03-24 15:52:29 -0600

    Documentation
    Signed-off-by: David Ward <david.a.ward@edu.sait.ca>

--- src/main/java/com/magtable/controller/UserController.java
+++ src/main/java/com/magtable/controller/UserController.java
@@ -22,7 +22,7 @@ import java.util.List;
 
 /**
  * REST controller for user management routes
- * @author David Ward
+ * @author David Ward, Mustafa Al Khaldi
  */
 @RestController
 @RequestMapping("/user")
