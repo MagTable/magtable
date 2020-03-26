f3bf96a6ed38e7524a8b472f4d7d9d556c807802
Steven Wong <stevenvono@gmail.com>
2020-03-26 05:31:29 -0600

    Attempting to fix the refresh

--- src/main/java/com/magtable/controller/ErrorController.java
+++ src/main/java/com/magtable/controller/ErrorController.java
@@ -2,14 +2,11 @@ package com.magtable.controller;
 
 import com.magtable.services.ErrorService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 public class ErrorController {
-
-    @Autowired
-    public ErrorService errorService;
 
     @GetMapping("/error")
     public String error() {
