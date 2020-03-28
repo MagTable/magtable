925914bbf1cd8188447c14f139c37ace0a921fae
Arran Woodruff <arranwoodruff@gmail.com>
2020-03-28 13:32:04 -0600

    additional styling
    fadein on load for employee/truck/tower/userlist divs
    removed slide down for parking locations to avoid frameyness

--- client/src/styled/user/User.js
+++ client/src/styled/user/User.js
@@ -1,4 +1,4 @@
-import styled from "styled-components";
+import styled, { keyframes } from "styled-components";
 import { isMobile } from "react-device-detect";
 
 /**
@@ -7,6 +7,15 @@ import { isMobile } from "react-device-detect";
  * @category Styled Components
  * @module User
  */
+
+const fadeIn = keyframes`
+  0% {
+    opacity: 0;
+  }
+  100% {
+    opacity: 1;
+  }
+`;
 
 /**
  * Holds all of the rows / information in the user list.
@@ -17,8 +26,12 @@ export const UserListDiv = styled.div`
 	width: 40vw;
 	max-height: calc(100vh - 70px);
 	padding: 1rem 2.5rem;
-	${isMobile &&  // Mobile rules
-		`padding-top: 5px;`}
+	animation: 0.3s ${fadeIn} ease-out;
+
+	${isMobile && // Mobile rules
+		`
+		padding-top: 5px;
+	`}
 `;
 
 export const UserListSection = styled.div`
@@ -89,11 +102,4 @@ export const UserManipulateBlock = styled.div`
 		  border-bottom: 2px solid #DEDEDE; 
 		  margin-bottom: 10px;`}
 	\`;
-`;
-
-export const SeparatorLine = styled.div`
-	border-bottom: 2px solid #dedede;
-	height: 0px;
-	width: auto;
-	margin-top: 20px;
 `;
