c36a7c0b0a5370dae5191568efa490b9d3f8c083
Arran Woodruff <arranwoodruff@gmail.com>
2020-04-02 20:18:48 -0600

    Update StompClient.js

--- client/src/components/common/StompClient.js
+++ client/src/components/common/StompClient.js
@@ -7,27 +7,15 @@ import { updateTable } from "../../actions/magtable";
 const StompClient = ({ setWSConnected }) => {
 	const dispatch = useDispatch();
 	const client = new Client({
-		brokerURL: "wss://sait-capstone2020.herokuapp.com/ws/websocket",
+		brokerURL: "ws://localhost:8080/ws/websocket",
+		// brokerURL: "wss://sait-capstone2020.herokuapp.com/ws/websocket",
 		reconnectDelay: 3000,
-		heartbeatIncoming: 2000,
-		heartbeatOutgoing: 2000
+		heartbeatIncoming: 10000,
+		heartbeatOutgoing: 10000
 	});
 
 	client.onWebSocketClose = () => {
 		setWSConnected(false);
-	};
-
-	client.debug = function(str) {
-		console.log(str);
-	};
-
-	client.onStompError = function(frame) {
-		// Will be invoked in case of error encountered at Broker
-		// Bad login/passcode typically will cause an error
-		// Complaint brokers will set `message` header with a brief message. Body may contain details.
-		// Compliant brokers will terminate the connection after any error
-		console.log("Broker reported error: " + frame.headers["message"]);
-		console.log("Additional details: " + frame.body);
 	};
 
 	client.onConnect = () => {
